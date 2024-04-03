5eff7eb1809d8112d5f0b90abde46e5e5148e447
nixuan123 <128340967+nixuan123@users.noreply.github.com>
2024-04-03 22:44:06 +0800

    Update event.cc

--- src/sst/core/event.cc
+++ src/sst/core/event.cc
@@ -19,7 +19,8 @@
 #include <sys/time.h>
 
 namespace SST {
-
+//id_counter是SST::Event类的一个静态成员变量，用于生成唯一的事件ID。它被声明为<uint64_t>类型
+//意味着它是一个原子类型的无符号64位整数
 std::atomic<uint64_t>     SST::Event::id_counter(0);
 const SST::Event::id_type SST::Event::NO_ID = std::make_pair(0, -1);
 
@@ -30,7 +31,7 @@ Event::execute(void)
 {
     (*reinterpret_cast<HandlerBase*>(delivery_info))(this);
 }
-
+//它的作用是创建并返回当前事件对象的一个副本
 Event*
 Event::clone()
 {
@@ -40,7 +41,8 @@ Event::clone()
         " implement it.");
     return nullptr; // Never reached, but gets rid of compiler warning
 }
-
+//此方法用于生成一个唯一的事件标识符，他接受两个参数，一个是id_counter的当前值
+//第二个是模拟中的当前排名
 Event::id_type
 Event::generateUniqueId()
 {
